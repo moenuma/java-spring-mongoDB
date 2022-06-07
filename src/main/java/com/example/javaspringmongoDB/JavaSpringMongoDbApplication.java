0ca10cfb06438e11c0841c9811783652c75bf1f8
moenuma <moenumasawa@gmail.com>
2022-06-07 21:00:08 +0900

    initial commit

---
+++ src/main/java/com/example/javaspringmongoDB/JavaSpringMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaspringmongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javaspringmongoDB/JavaSpringMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaspringmongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
